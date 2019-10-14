082a4d41808633403cacb42bf05657aa1a1ea9d4
Andrey Grach <andrejgrach@icloud.com>
2019-10-15 02:13:16 +0300

    Works fine. Animation lasted

---
+++ src/slider/Slider.vue
@@ -0,0 +1,106 @@
+<template>
+  <div id="slider">
+    <div class="arrowLeft" @click ="arrowLeft()"> </div>
+    <Slides :image="images[chosenImage]"/>
+    <div class="arrowRight" @click = "arrowRight()"></div>
+  </div>
+</template>
+
+<script>
+  import Slides from './Slides.vue';
+  export default {
+    components : {
+      Slides
+    },
+    data(){
+      return{
+        images : [
+          {
+            id: 0,
+            url: "/images/image1.jpg",
+            /*title: "Nassim_Taleb"*/
+          },
+          {
+            id: 1,
+            url: "/images/image2.jpg",
+            /*title: "Hall"*/
+          }
+
+        ],
+        chosenImage : 0,
+        intervalObject : null
+      }
+    },
+    methods :{
+      arrowLeft(){
+        clearInterval(this.intervalObject);
+        this.moveLeft();
+        var self = this;
+        this.intervalObject = setInterval(()=> {
+          self.moveLeft();
+        },4000);
+      },
+      arrowRight(){
+        clearInterval(this.intervalObject);
+        this.moveRight();
+        var self = this;
+        this.intervalObject = setInterval(()=> {
+          self.moveRight();
+        },4000);
+
+      },
+      moveLeft(){
+        var flag = this.chosenImage;
+        flag--;
+        if(flag < 0){
+          flag = (this.images.length -1);
+        }
+        this.chosenImage = flag;
+
+
+      },
+      moveRight(){
+        var flag = this.chosenImage;
+        flag++;
+        if (flag >= this.images.length){
+          flag = 0;
+        }
+
+        this.chosenImage = flag;
+      }
+
+    },
+
+    created(){
+      var self= this;
+      this.intervalObject = setInterval(()=>{
+        self.moveLeft();
+      },4000);
+    }
+  }
+</script>
+
+<style>
+  #slider{
+    position: relative;
+
+  }
+  #slider .arrowLeft, #slider .arrowRight {
+    position: absolute;
+    top: 50%;
+    border: 20px solid transparent;
+
+
+  }
+  #slider .arrowLeft {
+    border-right-color: black;
+    z-index: 1;
+  }
+
+  #slider .arrowRight {
+    right: 0;
+    border-left-color: black;
+  }
+
+
+</style>
---
+++ src/slider/Slides.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="slides">
+    <div class="slideImage" :style="setBackgroundImage">
+      </div>
+    </div>
+
+</template>
+
+<script>
+  export default {
+      props: ["image"],
+      computed:{
+          setBackgroundImage(){
+              return {backgroundImage: "url("+this.image.url+")"};
+          }
+      }
+
+  }
+</script>
+
+<style>
+  #slides{
+
+  }
+  #slides .slideImage{
+    height: 50vh;
+    width: 50%;
+    background-position:center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    /* background-image:url("/images/image1.jpg"); */
+  }
+  #slides .slideImage .slideTitle{
+    color: ;
+  }
+</style>
