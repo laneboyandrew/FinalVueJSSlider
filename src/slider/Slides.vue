c60cd34d4ce8313b890657a6a3fa20b1fd7da953
laneboyandrew <andrejgrach@icloud.com>
2019-10-15 11:14:45 +0300

    Add styles

---
+++ src/slider/Slides.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="slides">
+    <div class="slideImage" :style="setBackgroundImage">
+      </div>
+    </div>
+
+</template>
+
+<script>
+  export default {
+      props: ["image"],
+      computed:{
+          setBackgroundImage(){
+              return {backgroundImage: "url("+this.image.url+")"};
+          }
+      }
+
+  }
+</script>
+
+<style>
+  #slides{
+
+  }
+  #slides .slideImage{
+    height: 60vh;
+    width: 50%;
+    background-position:center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    position: absolute;
+    margin-left: 26%;
+    margin-top: 5%;
+  }
+
+</style>
